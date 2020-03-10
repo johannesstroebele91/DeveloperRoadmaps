79d1071688ea88d78750a8b3fb5d99ada8b55047
JohannesStroebele <stroebele.j@gmail.com>
2020-03-10 17:44:35 +0100

    Implemented responsive header navigation, fixed routing, added text stylings, and added basic pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
-import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {FlexLayoutModule} from '@angular/flex-layout';
-
 import {MatCheckboxModule} from '@angular/material/checkbox';
+
+import {HeaderComponent} from './header/header.component';
+import {HomeComponent} from './pages/home/home.component';
+import {FrontendComponent} from './pages/frontend/frontend.component';
+import {BackendComponent} from './pages/backend/backend.component';
+import {AboutComponent} from './pages/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FrontendComponent,
+    BackendComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
     FlexLayoutModule,

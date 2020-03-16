d50de2ed51b635953c7e88bc5f11f758e7ce8f2e
JohannesStroebele <stroebele.j@gmail.com>
2020-03-16 19:05:32 +0100

    Fixed mobile navigation, added minor text improvements, and added content for frontend road map

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {FlexLayoutModule} from '@angular/flex-layout';
-import {MatCheckboxModule} from '@angular/material/checkbox';
+import {RouterModule} from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
+import {HttpClientModule} from '@angular/common/http';
 
 import {HeaderComponent} from './header/header.component';
 import {HomeComponent} from './pages/home/home.component';
 import {FrontendComponent} from './pages/frontend/frontend.component';
 import {BackendComponent} from './pages/backend/backend.component';
 import {AboutComponent} from './pages/about/about.component';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @NgModule({
   declarations: [
@@ -19,13 +22,16 @@ import {AboutComponent} from './pages/about/about.component';
     FrontendComponent,
     BackendComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
   ],
   imports: [
+    RouterModule,
+    HttpClientModule,
     AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
+    MatIconModule,
     MatCheckboxModule
   ],
   providers: [],
--- src/app/pages/frontend/frontend.component.ts
+++ src/app/pages/frontend/frontend.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+// import * as milestonesFrontend from '../../../assets/frontend-roadmap-content.json';
+import {FrontendDataService} from '../../frontend-data.service';
 
 @Component({
   selector: 'app-frontend',
   templateUrl: './frontend.component.html',
   styleUrls: ['./frontend.component.scss']
 })
+
 export class FrontendComponent implements OnInit {
 
-  constructor() { }
+  milestonesFrontendContent: any;
+  milestonesFrontendHeadlines: string[];
 
-  ngOnInit(): void {
+  constructor(private frontendDataService: FrontendDataService) {
+    // console.log('Reading local json files');
+    // console.log(milestonesFrontend);
   }
 
+  ngOnInit() {
+    this.frontendDataService.getMilestonesFrontendContent().subscribe(data => {
+      this.milestonesFrontendContent = data;
+    });
+    this.frontendDataService.getMilestonesFrontendHeadlines().subscribe(data => {
+      this.milestonesFrontendHeadlines = data;
+    });
+  }
 }
